8490f27bfdc08b3925767d34b3fb2ace19bd1402
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2023-07-17 14:49:38 +0100

    display all the articles, add responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,22 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import ArticleList from './components/ArticleList'
+import { Route, Routes } from 'react-router'
+import TopicPage from './components/TopicPage'
 
 function App() {
 
   return (
     <div className='app'>
       <Navbar/>
-      <div className='text-center text-red-600 font-black'>
+      <div className='text-center text-red-600 font-black mt-10 text-2xl'>
         <h1>Welcome to NC-news</h1>
       </div>
-      <ArticleList/>
+
+      <Routes>
+        <Route path='/' element={<ArticleList/>}/>
+        <Route path='/topic' element={<TopicPage/>}/>
+      </Routes>
       <Footer/>
     </div>
   )
--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,16 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ArticleCard from './ArticleCard'
-
+import fetchAllArticles from "../utils/api"
 const ArticleList = () => {
-    const [articles, setArticles] = useState([
-    ])
+  const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+    const getAllArticles = async () => {
+      const articles = await fetchAllArticles()
+      setArticles(articles)
+    }
+    getAllArticles()
+  }, [])
+  
   return (
-    <div className='border border-black w-4/5 m-auto p-4'>
-        <ArticleCard/>
-        <ArticleCard/>
-        <ArticleCard/>
-        <ArticleCard/>
-        <ArticleCard/>
+    <div className='grid gap-4 w-4/5 m-auto mt-5 p-4 max-sm:grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+      {articles.map(({title, author, topic, votes, article_id, article_img_url}) => {
+        return <ArticleCard key={article_id} title={title} author={author} topic={topic} votes={votes} article_img_url={article_img_url}/>
+      })}
+      
     </div>
   )
 }
