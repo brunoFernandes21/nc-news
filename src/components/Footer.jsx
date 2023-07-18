e1e3671acf25c11d8b993956e733acc8dc2f4b68
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2023-07-18 10:08:49 +0100

    adjusted footer height

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <div className=" w-full mt-28 py-5 flex justify-center items-center text-white bg-red-600 font-black">
+    <div className=" w-full mt-20 py-5 flex justify-center items-center text-white bg-red-600 font-black">
         <p>Bruno Fernandes &copy; All right reserved.</p>
     </div>
   )
