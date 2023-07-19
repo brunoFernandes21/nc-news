a6a416ee1cbde1a42a1cbc37695eb6293da30113
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2023-07-19 17:53:21 +0100

    add page not found

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,18 @@ import TopicPage from "./components/TopicPage";
 import SingleArticle from "./components/SingleArticle";
 import { useContext } from 'react';
 import { ThemeContext } from './contexts/Theme';
+import NotFound from "./components/NotFound";
 function App() {
   const { theme } = useContext(ThemeContext)
-
+ 
   return (
     <div className={`app h-screen flex flex-col justify-between ${theme}`}>
       <Navbar />
       <Routes>
         <Route path="/" element={<ArticleList />} />
         <Route path="/articles/:article_id" element={<SingleArticle />} />
-        <Route path="/topic" element={<TopicPage />} />
+        <Route path="/article/:topic" element={<TopicPage/>} />
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
       <Footer />
     </div>
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useContext } from 'react';
+import { ThemeContext } from '../contexts/Theme';
+const NotFound = () => {
+    const { theme } = useContext(ThemeContext)
+
+  return (
+    <div className={`p-10 font-bold block text-center m-auto ${theme === "dark" ? "text-white" : "text-red-500"}`}>
+        <h1 className='text-8xl'>404</h1>
+        <h2 className='text-4xl'>Page Not Found</h2>
+        <p className='text-xl'>Go to the <Link className='underline' to={'/'}>Homepage</Link>.</p>
+    </div>
+  )
+}
+
+export default NotFound
