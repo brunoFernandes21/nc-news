96d910611482786fd416480713dd5e3154a383ff
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2023-07-19 09:12:04 +0100

    allow user to vote an article and add some more functionality

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -2,10 +2,12 @@ import { useParams } from "react-router-dom";
 import CommentCard from "./CommentCard";
 import { useState, useEffect } from "react";
 import { fetchCommentsByArticleId } from "../utils/api";
-
+import { useContext } from 'react';
+import { ThemeContext } from '../contexts/Theme';
 const CommentList = ({ article_id }) => {
   const [comments, setComments] = useState([]);
   const [loading, setLoading] = useState(true);
+  const { theme } = useContext(ThemeContext)
 
   useEffect(() => {
     setLoading(true);
@@ -19,21 +21,21 @@ const CommentList = ({ article_id }) => {
   return (
     <div>
       {loading && (
-        <h2 className="text-center text-red-600 font-black mt-10 text-2xl md:text-4xl">
+        <h2 className={`${theme === "dark" ? "text-white" : "text-red-600"} text-center  font-black mt-10 text-2xl md:text-4xl`}>
           Loading Comments...
         </h2>
       )}
       {!loading && (
-        <main>
-          <section className=" border border-red-600 m-auto text-center mt-10 text-red-600 font-black text-xl md:text-4xl">
+        <main className="bg-blue" >
+          <section className={`${theme === "dark" ? "text-white border border:white" : "border border-black  text-black "} m-auto text-center mt-10 p-2 md:p-4 font-black text-xl md:text-4xl`}>
             <h2>Comments</h2>
           </section>
           {comments.length === 0 ? (
-            <h3 className="m-auto text-center mt-10 text-red-600 text-xl md:text-4xl">
+            <h3 className={`${theme === "dark" ? "text-white" : "text-black"} m-auto text-center mt-10 text-xl md:text-4xl`}>
               No comments yet!
             </h3>
           ) : (
-            <section className="grid gap-4 m-auto mt-10 md:grid-cols-2">
+            <section className="grid mb-10 gap-4 m-auto mt-10 md:grid-cols-2">
               {comments.map(({ author, votes, body }) => {
                 return (
                   <CommentCard
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </BrowserRouter>
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./contexts/Theme";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ThemeProvider>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </ThemeProvider>
+);
