b5c5194d25487e10aa692b3b67e56a3e8846ad15
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2023-07-23 10:17:51 +0100

    increase footer text size for medium to large devices

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return new Date().getFullYear();
 }
   return (
-    <footer className="text-sm w-full lg:mt-0 py-5 flex justify-center items-center text-white bg-red-600 font-black">
+    <footer className="text-sm md:text-lg w-full lg:mt-0 py-5 flex justify-center items-center text-white bg-red-600 font-black">
         <p >Bruno Fernandes &copy; {getYear()} All Rights Reserved</p>
     </footer>
   )
